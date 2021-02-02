85695483d3fe11fe7fa4d015a4e2bf0752007340
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-03 02:27:40 +0400

    Update mongooseCon.controller.js

--- controlers/mongooseCon.controller.js
+++ controlers/mongooseCon.controller.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const URL='mongodb://localhost:27017/DB'
+const URL="mongodb+srv://example:example123@cluster0.mp4bd.mongodb.net/export?authSource=admin&replicaSet=atlas-4kvxp2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" || 'mongodb://localhost:27017/DB'
 
 const connection = mongoose.connect(URL, {
   useNewUrlParser: true,
