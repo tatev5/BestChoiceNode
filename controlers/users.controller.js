b374c6a2ce4952549fa20b6e339dbb9f16a5d750
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-03 02:12:00 +0400

    Update users.controller.js

--- controlers/users.controller.js
+++ controlers/users.controller.js
@@ -1,47 +1,41 @@
 const User = require('../models/users.model');
-
-
-
-
+const jwt = require('jsonwebtoken');
 class UserController {
     static async getAll(req, res) {
         const info = await User.find();
-        let login = req.query.name;
-        let pass = req.query.password;
-        
-        for(let i = 0; i < info.length; i++){
-           if(info[i].name === login && info[i].password === pass){
-              res.json(info[i]);
-           }
-        }
-        
-    }
-
-    static async getOne(req, res){
-        console.log(req.query,"3");
-        const info = await User.findById(req.params.id);
         return info;
     }
-    
-    static async create(req, res) {
-        console.log(req.body,"3");
-        if(req.query.password===req.query.confirmPassword && req.query.name && req.query.lastname && req.query.email){
-            const info = await User.create(req.query);
-            console.log(req.query)
-            res.json(info)
+    static async getOne(req, res) {
+        let login = req.query.name;
+        let pass = req.query.password;
+        if(login && pass){
+            const info = await User.findOne({ name: login });
+            if(info && info.password === pass){
+                const token = jwt.sign({name: info.name, email: info.email}, "asdddd");
+                return res.send({token});
+            }
         }
-        
+        return res.status(400).json({message: "Incorrect login or password"});
     }
-    
+    static async create(req, res) {
+       const { name, lastname, email, password} = req.body;
+       const UserInfo = await User.findOne({ email: email });
+       if(UserInfo){
+            return res.status(400).json({message: "Incorrect login or password"});
+       }
+       else if(name && lastname && email && password){
+        const newUser = await User.create(req.body);
+            return console.log(req.body);
+       }
+       return res.status(400).json({message: "Incorrect login or password"})
+    }
     static async update(req, res) {
         const info = await User.findByIdAndUpdate(itemId, data);
         return info;
     }
-    
     static async remove(req, res) {
         const info = await User.findByIdAndRemove(itemId);
         return info;
     }
 }
-
 module.exports = UserController;
