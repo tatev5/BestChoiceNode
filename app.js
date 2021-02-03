0b5b2a417376321bb663711cf26bf4491c362cf3
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-04 01:38:08 +0400

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/submit', usersRouter);
+app.use('/login', usersRouter);
 
 
 
