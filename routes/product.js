736dc83f70ce96273fa217e9c9749dd817849a3e
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-05 01:47:22 +0400

    Add files via upload

---
+++ routes/product.js
@@ -0,0 +1,37 @@
+router.post("/cart", async (req, res) => {
+  const { productId, quantity, name, price } = req.body;
+
+  const userId = "5de7ffa74fff640a0491bc4f"; //TODO: the logged in user id
+
+  try {
+    let cart = await Cart.findOne({ userId });
+
+    if (cart) {
+      //cart exists for user
+      let itemIndex = cart.products.findIndex(p => p.productId == productId);
+
+      if (itemIndex > -1) {
+        //product exists in the cart, update the quantity
+        let productItem = cart.products[itemIndex];
+        productItem.quantity = quantity;
+        cart.products[itemIndex] = productItem;
+      } else {
+        //product does not exists in cart, add new item
+        cart.products.push({ productId, quantity, name, price });
+      }
+      cart = await cart.save();
+      return res.status(201).send(cart);
+    } else {
+      //no cart for user, create new cart
+      const newCart = await Cart.create({
+        userId,
+        products: [{ productId, quantity, name, price }]
+      });
+
+      return res.status(201).send(newCart);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+});
