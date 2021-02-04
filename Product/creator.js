9f1a1dbf11fc636db52bfb8141839d8a36b24fc8
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-05 01:42:27 +0400

    Add files via upload

---
+++ Product/creator.js
@@ -0,0 +1,24 @@
+const productSchema = require("./Product");
+const mongoURL = require('./mongooseCon.controller');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(res => {
+  console.log('Connected!');
+}).catch(err => {
+  return console.error(err.message);
+})
+
+const productSchema = new Schema({
+    name:String,
+    brand:String,
+    price: Number, 
+    description:String,
+    featured: Boolean,
+
+ });
+
+module.exports= mongoose.model("Product", productSchema);
---
+++ Product/product.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    products: [
+      {
+        productId: Number,
+        quantity: Number,
+        name: String,
+        price: Number
+      }
+    ],
+    active: {
+      type: Boolean,
+      default: true
+    },
+    modifiedOn: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
+
+    
+    
+    
