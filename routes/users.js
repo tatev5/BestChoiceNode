8a0c69085cce73e9a835f08a8bd81dd5f64f7ceb
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-05 19:51:15 +0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const UserController = require('../controlers/users.controller');
-const Product = require('../Product/product');
+
  
 /* GET users listing. */
 router.post('/',UserController.create);
 router.post('/login', UserController.getOne); 
 router.put('/:id', UserController.update);
 router.delete('/:id', UserController.remove);
-
-/* GET product listing. */
-router.get('/', Product.getOne);
 
 module.exports = router;
