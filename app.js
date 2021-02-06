dc369908549abd6ccae0c67e35c5df62097fd49a
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-07 03:02:37 +0400

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(bodyParser.json()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
