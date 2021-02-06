ea2fe64204acfe9db9655a33dfe912f48fe48bfd
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-07 02:19:36 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser=require("body-parser");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const connectMongoose = require("./controlers/mongooseCon.controller");
-const bodyParser=require("body-parser");
+const authorization = require("./middlewares/authorizat");
+
 
 
 
@@ -20,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use(authorization);
 
 
 
---
+++ middlewares/authorizat.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const users = require("../models/users.model");
+
+
+async function authorizat (req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, '123asdasd');
+    console.log(decoded);
+    const user = await users.findOne({email: decoded.email});
+    if (user) {
+      res.json(user);
+      return next();
+    }
+    return res.status(403).json({message: "Not Verified User"});
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+
+}
+
+module.exports = authorizat;
