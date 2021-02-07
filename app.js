0b7a75744c8eaf221d81f587a96f6e0ca45855d2
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-07 17:44:15 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser=require("body-parser");
-
+const cors =require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const connectMongoose = require("./config.js");
