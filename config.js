7ac82b8d9dddf5249425a3bd3c61e2b31d8f0704
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-07 04:12:18 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    mongodburl: process.env.DB_URL
-}
+const mongoose = require("mongoose");
+const URL="mongodb+srv://example:example123@cluster0.mp4bd.mongodb.net/export?authSource=admin&replicaSet=atlas-4kvxp2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" || 'mongodb://localhost:27017/DB'
+
+const connection = mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(res => {
+  console.log("connected DB");
+}).catch(err => {
+  console.error(err.message);
+});
+
+module.exports = connection;
